32fe3ba46cd0a4b3ddae16c319451174bc8eac3a
wangyouhe.ezio <wangyouhe.ezio@bytedance.com>
2022-06-27 13:47:03 +0800

    add gin

--- app/main.go
+++ app/main.go
@@ -1,9 +1,18 @@
 package main
 
-import client "app/client"
+import (
+	client "app/client"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	println("hello world")
-
 	client.Client()
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello World!")
+	})
+	r.Run(":8888")
 }
